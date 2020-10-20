4e55ad6aaf5b912a46f20625df2b3f2a59f4bd42
H4WG <shyter2012@gmail.com>
2020-10-20 17:34:34 +0300

    Оптимізація коду, результат виконання та блок схема

--- Лабораторна робота #5/Завдання #1. Код програми.cpp
+++ Лабораторна робота #5/Завдання #1. Код програми.cpp
@@ -13,61 +13,55 @@ using namespace std;
 
 int main()
 {
-	int Sum_Positive_Values = 0;   // Сума позитивних значень.
-	int Count_Positive_Values = 0; // Кількість позитивних значень.
-	int Average_Values = 0;        // Середнє значення.
-	int arr[20][10];               // Масив для збереження згенерованих чисел.
+	int Sum_Positive_Values;   // Сума позитивних значень.
+	int Count_Positive_Values; // Кількість позитивних значень.
+	int Average_Values[20];    // Середнє значення.
+	int arr[20][10];           // Масив для збереження згенерованих чисел.
 
-	srand(time(NULL));	       // Ініціалізація rand().
+	srand(time(NULL));	   // Ініціалізація rand().
 
 	cout << "\nЗдійснюємо генерацію самого масиву (без змін).\n\n";
 
 	for(int i = 0; i <= 19; i++) {
 
-		// Нумерація для зручності.
+		Sum_Positive_Values = 0;
+		Count_Positive_Values = 0;
+
 		cout << setw(2) << i + 1 << ": ";
 
 		// Генерація чисел та збереження у їх у масив.
 		for(int j = 0; j <= 9; j++) {
 			arr[i][j] = rand() % 201 -100;
 			cout << setw(5) << arr[i][j];
-		}
 
-		cout << endl;
-	}
-
-	cout << "\nВиведення редагованого масиву (згідно зі завданням).\n\n";
-
-	for(int i = 0; i <= 19; i++) {
-
-		cout << setw(2) << i + 1 << ": ";
-
-		// В данному циклі шукаємо середнє значення.
-		for(int j = 0; j <= 9; j++) {
-
-			// Визначаємо, чи позитивне значення,
-			// якщо ні, то беремо наступне число.
+			// Шукаємо середнє значення з позитивних значень.
 			if( 0 < arr[i][j] ) {
 				Sum_Positive_Values += arr[i][j];
 				Count_Positive_Values++;
 			}
 		}
 
-		Average_Values = Sum_Positive_Values / Count_Positive_Values;
+		Average_Values[i] = Sum_Positive_Values / Count_Positive_Values;
+
+		cout << "  |  " <<  Average_Values[i] << endl;
+	}
+
+	cout << "\nВиведення редагованого масиву (згідно зі завданням).\n\n";
+
+	for(int i = 0; i <= 19; i++) {
+		cout << setw(2) << i + 1 << ": ";
 
 		// Використовуємо середнє значення для заміни
 		// позитивного значення, яке є менше середнього.
-
 		for(int j = 0; j <= 9; j++) {
-			if( arr[i][j] < Average_Values && 0 < arr[i][j] ) {
-				cout << setw(5) << Average_Values;
+			if( arr[i][j] < Average_Values[i] && -1 < arr[i][j] ) {
+				cout << setw(5) << Average_Values[i];
 			} else {
 				cout << setw(5) << arr[i][j];
 			}
 		}
 
-		// Виведення середнього значення для зручності.
-		cout << "  |  " <<  Average_Values << endl;
+		cout << "  |  " <<  Average_Values[i] << endl;
 	}
 
 	cout << endl;
