023b7fbc4e10d5f9a7eba357b31b00f47939bc7d
H4WG <shyter2012@gmail.com>
2020-12-10 01:43:34 +0200

    Додавання лабораторної роботи #9

---
+++ Лабораторна робота #9/Код програми.cpp
@@ -0,0 +1,32 @@
+// Лабораторна робота #9. Варіант #15.
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	// Зчитуємо файл data.txt.
+	ifstream data;
+	data.open("data.txt");
+
+	string text;
+
+        cout << setw(9) << "ОС";
+	cout << setw(16) << "СУБД";
+	cout << setw(7) << "ROM";
+	cout << setw(5) << "RAM";
+        cout << setw(12) << "Ціна\n\n";
+
+        while (getline(data, text)) {
+		cout << text << endl;
+	}
+
+	// Закриваємо файл.
+	data.close();
+
+	return 0;
+}
